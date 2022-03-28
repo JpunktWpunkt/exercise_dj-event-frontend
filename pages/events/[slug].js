4a93d1421b45b2d4e105b491fe16c9730edee03e
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-03-28 13:48:47 +0200

    add Link & Router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,13 @@
-import React from 'react';
+import {useRouter} from "next/router";
 
 const EventPage = () => {
+    const router = useRouter() //create a const router to use Router ^^
+
+    console.log(router) //a lot of information about p.e. pathname usw....
     return (
         <div>
             <h1>My Event</h1>
+            <h3>{router.query.slug}</h3>
         </div>
     );
 }
