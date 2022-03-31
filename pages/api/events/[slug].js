69e5f73b3665f985b74ae02bdf3e6adc27eef240
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-03-31 22:00:45 +0200

    add api event slug

---
+++ pages/api/events/[slug].js
@@ -0,0 +1,14 @@
+const {events} = require('./data.json');
+//limit the methods who can
+export default function handler(request, response) {
+    const evt = events.filter(event => event.slug === req.query.slug);
+
+
+    if (request.method === 'GET') {
+
+        response.status(200).json({evt})
+    } else {
+        response.setHeader('Allow', [Get]); //will be to to force only the method that i would allowed, in this place only posible use Get, ansonsten gibt es eine Fehlermeldung
+        response.status(405).json({message: 'Method ${request.method} not supported'})
+    }
+}
