a6d42c86ec318e2a79e8fa0b718ea66180dc8a2c
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-04-01 18:12:45 +0200

    ad cfg files for api routing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "@/components/Layout";
+import {API_URL} from '@/config/index';
 
 export default function HomePage() {
     return (
@@ -10,7 +11,18 @@ export default function HomePage() {
                     <meta name="description" content="Welcome to DJ Events"/>
                 </title>
             </Head>
-            <h1>Home</h1>
+            <h1>Upcoming Events</h1>
         </Layout>
     )
+
+}
+
+
+export async function getServerSideProps() {
+    const response = await fetch(`${API_URL}/api/events`)
+    const events = await response.json()
+    console.log(events)
+    return {
+        props: {},
+    }
 }
