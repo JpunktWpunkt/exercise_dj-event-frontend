b1c794fed63493718cc8d0f30416ca24499b349d
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-04-02 12:19:07 +0200

    add Data Fetching with API Routing

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
-import Layout from "@/components/Layout";
-import {API_URL} from '@/config/index';
+import Layout from "@/components/Layout";//this way would be configure in jsconfig.json
+import {API_URL} from '@/config/index'; //this way would be configure in jsconfig.jso
 
-export default function HomePage(events) {
+export default function HomePage({events}) {
+    console.log(events)
     return (
         <Layout>
             <Head>
@@ -12,17 +13,21 @@ export default function HomePage(events) {
                 </title>
             </Head>
             <h1>Upcoming Events</h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+            {events.map(evt => (<h3 key={evt.id}>{evt.name}</h3>
+            ))}
         </Layout>
     )
 
 }
 
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
     const response = await fetch(`${API_URL}/api/events`)
     const events = await response.json()
     console.log(events)
     return {
         props: {events},
+        revalidate: 1,
     }
 }
