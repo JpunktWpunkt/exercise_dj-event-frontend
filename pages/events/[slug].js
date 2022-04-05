1eaf4ea66c3332edf2d917522e26650e3f7797d2
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-04-05 11:36:44 +0200

    add getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,14 @@
 import {useRouter} from "next/router";
 import Layout from '../../components/Layout';
+import {API_URL} from '/config/index';
 
-const EventPage = () => {
+const EventPage = ({evt}) => {
     const router = useRouter() //create a const router to use Router ^^
 
     console.log(router) //a lot of information about p.e. pathname usw....
     return (
         <Layout>
-            <h1>My Event</h1>
+            <h1>{evt.name}</h1>
             <h3>{router.query.slug}</h3>
         </Layout>
     );
@@ -15,3 +16,47 @@ const EventPage = () => {
 
 
 export default EventPage;
+
+export async function getStaticPaths() {
+    const response = await fetch(`${API_URL}/api/events `)
+    const events = await response.json()
+
+    const paths = events.map((evt) => ({
+        params: {slug: evt.slug}
+    }))
+    return {
+        paths,
+        fallback: true,
+    }
+}
+
+export async function getStaticProps({params: {slug}}) {
+    console.log(slug)
+    const response = await fetch(`${API_URL}/api/events/${slug}`)
+    const events = await response.json()
+
+
+    return {
+        props: {
+            evt: events [0]
+        },
+        revalidate: 1
+
+    }
+}
+
+
+/*
+export async function getServerSideProps({query: {slug}}) {
+    console.log(slug)
+    const response = await fetch(`${API_URL}/api/events/${slug}`)
+    const events = await response.json()
+
+
+    return {
+        props: {
+            evt: events [0]
+        },
+
+    }
+}*/
