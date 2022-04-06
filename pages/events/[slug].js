b3eec896b1e9435442653b84ec9a6db4d0c29086
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-04-06 20:31:13 +0200

    add the detail event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,15 +1,52 @@
 import {useRouter} from "next/router";
-import Layout from '../../components/Layout';
-import {API_URL} from '/config/index';
+import Layout from '@/components/Layout';
+import {API_URL} from '@/config/index';
+import Link from 'next/Link';
+import {FaPencilAlt, FaTimes} from 'react-icons/fa';
+import styles from '@/styles/Event.module.css'
+import Image from 'next/image';
 
 const EventPage = ({evt}) => {
     const router = useRouter() //create a const router to use Router ^^
-
+    const deleteEvent = (event) => {
+        console.log('delete')
+    }
     console.log(router) //a lot of information about p.e. pathname usw....
     return (
         <Layout>
-            <h1>{evt.name}</h1>
+            <div className="styles.event">
+                <div className="styles.controls">
+                    <Link href={`/events/edit/{evt.id}`}>
+                        <a>
+                            <FaPencilAlt/> Edit Event
+                        </a>
+                    </Link>
+                    <a href="#" className={styles.delete}
+                       onClick={deleteEvent}>
+                        <FaTimes/> Delete Event
+                    </a>
+                </div>
+            </div>
             <h3>{router.query.slug}</h3>
+            <span>
+                {evt.date} at {evt.time}
+            </span>
+            <h1>{evt.name}</h1>
+            {evt.image && (
+                <div className={styles.image}>
+                    <Image src={evt.image} width={960} height={600}/>
+                </div>
+            )}
+            <h3>Performers:</h3>
+            <p>{evt.performers}</p>
+            <h3>Description:</h3>
+            <p>{evt.description}</p>
+            <h3>Venue: {evt.venue}</h3>
+            <p>{evt.adress}</p>
+
+            <Link href="/events">
+                <a className={styles.back}>Go back</a>
+            </Link>
         </Layout>
     );
 }
